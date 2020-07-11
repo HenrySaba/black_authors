d4ef9bfbc5c82dda218c38bd123d1b6850de5b76
HenrySaba <henry-saba@hotmail.com>
2020-07-11 00:44:45 -0400

    Added goodreads href links

--- javascript.js
+++ javascript.js
@@ -10,7 +10,8 @@ var books = [
       '\n' +
       'With “fresh and honest” (Jojo Moyes) prose, Queenie is a remarkably relatable exploration of what it means to be a modern woman searching for meaning in today’s world.',
     purchase_url: 'https://tinyurl.com/yd2lfkxp',
-    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1580245350-51KGeS2os3L.jpg?crop=1.00xw:0.968xh;0,0.0140xh&resize=768:*'
+    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1580245350-51KGeS2os3L.jpg?crop=1.00xw:0.968xh;0,0.0140xh&resize=768:*',
+    goodreads_url: 'https://www.goodreads.com/book/show/36586697-queenie?from_search=true&from_srp=true&qid=NhCidbsmKm&rank=1'
   },
   {
     title: 'Between the World and Me',
@@ -21,7 +22,8 @@ var books = [
       ' \n' +
       'Between the World and Me is Ta-Nehisi Coates’s attempt to answer these questions in a letter to his adolescent son. Coates shares with his son—and readers—the story of his awakening to the truth about his place in the world through a series of revelatory experiences, from Howard University to Civil War battlefields, from the South Side of Chicago to Paris, from his childhood home to the living rooms of mothers whose children’s lives were taken as American plunder. Beautifully woven from personal narrative, reimagined history, and fresh, emotionally charged reportage, Between the World and Me clearly illuminates the past, bracingly confronts our present, and offers a transcendent vision for a way forward.',
     purchase_url: 'https://www.amazon.com/dp/0812993543/ref=dp-kindle-redirect?_encoding=UTF8&btkr=1',
-    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1549482990-51ndtpPOGTL.jpg?crop=1xw:1xh;center,top&resize=768:*'
+    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1549482990-51ndtpPOGTL.jpg?crop=1xw:1xh;center,top&resize=768:*',
+    goodreads_url: 'https://www.goodreads.com/book/show/25489625-between-the-world-and-me?from_search=true&from_srp=true&qid=rxUQeyVXhZ&rank=1'
   },
   {
     title: 'We Should All Be Feminists',
@@ -32,7 +34,8 @@ var books = [
       '\n' +
       'Argued in the same observant, witty and clever prose that has made Adichie a bestselling novelist, here is one remarkable author’s exploration of what it means to be a woman today—and an of-the-moment rallying cry for why we should all be feminists.',
     purchase_url: 'https://www.amazon.com/We-Should-All-Be-Feminists/dp/110191176X/ref=sr_1_1?crid=2A2J97JC77LH3&dchild=1&keywords=we+should+all+be+feminists+chimamanda+ngozi+adichie&qid=1593461491&sprefix=we+should+all+be+femin%2Caps%2C156&sr=8-1',
-    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1549483007-51L9u20bC7L.jpg?crop=0.952xw:1xh;center,top&resize=768:*'
+    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1549483007-51L9u20bC7L.jpg?crop=0.952xw:1xh;center,top&resize=768:*',
+    goodreads_url: 'https://www.goodreads.com/book/show/22738563-we-should-all-be-feminists?ac=1&from_search=true&qid=Zxo7GjYHyi&rank=1'
   },
   {
     title: 'How We Fight for Our Lives',
@@ -45,33 +48,37 @@ var books = [
       '\n' +
       'Blending poetry and prose, Jones has developed a style that is equal parts sensual, beautiful, and powerful—a voice that’s by turns a river, a blues, and a nightscape set ablaze. How We Fight for Our Lives is a one of a kind memoir and a book that cements Saeed Jones as an essential writer for our time.',
     purchase_url: 'https://www.amazon.com/How-We-Fight-Our-Lives/dp/1501132733/ref=sr_1_1?crid=AOOPMFA72B4A&dchild=1&keywords=saeed+jones%2C+how+we+fight+for+our+lives&qid=1593461730&sprefix=saeed+jones%2Caps%2C170&sr=8-1',
-    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1580247216-51LfBgmusKL.jpg?crop=1.00xw:0.986xh;0,0.0140xh&resize=768:*'
+    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1580247216-51LfBgmusKL.jpg?crop=1.00xw:0.986xh;0,0.0140xh&resize=768:*',
+    goodreads_url: 'https://www.goodreads.com/book/show/43682552-how-we-fight-for-our-lives?from_search=true&from_srp=true&qid=XClehD84ft&rank=1'
   },
   {
     title: 'Black Leopard, Red Wolf',
     author: 'Marlon James',
     description: "In the first novel in Marlon James's Dark Star trilogy, myth, fantasy, and history come together to explore what happens when a mercenary is hired to find a missing child. \n" +
       '\n' +
-      'Tracker is known far and wide for his skills as a hunter: "He has a nose," people say. Engaged to track down a mysterious boy who disappeared three years earlier, Tracker breaks his own rule of always working alone when he finds himself part of a group that comes together to search for the boy. The band is a hodgepodge, full of unusual characters with secrets of their own, including a shape-shifting man-animal known as Leopard.\n' +
+      `Tracker is known far and wide for his skills as a hunter: '"'He has a nose," people say. Engaged to track down a mysterious boy who disappeared three years earlier, Tracker breaks his own rule of always working alone when he finds himself part of a group that comes together to search for the boy. The band is a hodgepodge, full of unusual characters with secrets of their own, including a shape-shifting man-animal known as Leopard.\n` +
       '\n' +
       "Drawing from African history and mythology and his own rich imagination, Marlon James has written an adventure that's also an ambitious, involving read. Defying categorization and full of unforgettable characters, Black Leopard, Red Wolf explores the fundamentals of truths, the limits of power, the excesses of ambition, and our need to understand them all.",
     purchase_url: 'https://www.amazon.com/Black-Leopard-Wolf-Dark-Trilogy/dp/0735220182/ref=sr_1_1?dchild=1&keywords=Black+Leopard%2C+Red+Wolf&qid=1593461928&sr=8-1',
-    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1549481591-61N2BU0SnDaL.jpg?crop=1xw:1xh;center,top&resize=768:*'
+    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1549481591-61N2BU0SnDaL.jpg?crop=1xw:1xh;center,top&resize=768:*',
+    goodreads_url: 'https://www.goodreads.com/book/show/50608676-black-leopard-red-wolf?ac=1&from_search=true&qid=LPUdvtMY9x&rank=1'
   },
   {
     title: 'Well-Read Black Girl',
     author: 'Glory Edim',
     description: 'An inspiring collection of essays by black women writers, curated by the founder of the popular book club Well-Read Black Girl, on the importance of recognizing ourselves in literature.\n' +
       '\n' +
-      `Remember that moment when you first encountered a character who seemed to be written just for you? That feeling of belonging can stick with readers the rest of their lives--but it doesn't come around as frequently for all of us. In this timely anthology, "well-read black girl" Glory Edim brings together original essays by some of our best black female writers and creative voices to shine a light on how we search for ourselves in literature, and how important it is that everyone--no matter their gender, race, religion, or abilities--can find themselves there. Whether it's learning about the complexities of femalehood from Their Eyes Were Watching God, seeing a new type of love in The Color Purple, or using mythology to craft an alternative black future, each essay reminds us why we turn to books in times of both struggle and relaxation. As she has done with her incredible book-club-turned-online-community Well-Read Black Girl, in this book, Edim has created a space where black women's writing and knowledge and life experiences are lifted up, to be shared with all readers who value the power of a story to help us understand the world, and ourselves.\n` +
+      `Remember that moment when you first encountered a character who seemed to be written just for you? That feeling of belonging can stick with readers the rest of their lives--but it doesn't come around as frequently for all of us. In this timely anthology, '"'well-read black girl" Glory Edim brings together original essays by some of our best black female writers and creative voices to shine a light on how we search for ourselves in literature, and how important it is that everyone--no matter their gender, race, religion, or abilities--can find themselves there. Whether it's learning about the complexities of femalehood from Their Eyes Were Watching God, seeing a new type of love in The Color Purple, or using mythology to craft an alternative black future, each essay reminds us why we turn to books in times of both struggle and relaxation. As she has done with her incredible book-club-turned-online-community Well-Read Black Girl, in this book, Edim has created a space where black women's writing and knowledge and life experiences are lifted up, to be shared with all readers who value the power of a story to help us understand the world, and ourselves.\n` +
       '\n' +
       'Contributors include: Jesmyn Ward (Sing Unburied Sing), Lynn Nottage (Sweat), Jacqueline Woodson (Another Brooklyn), Gabourey Sidibe (This Is Just My Face), Morgan Jerkins (This Will Be My Undoing), Zinzi Clemmons (What We Lose), N. K. Jemisin (The Fifth Season), Tayari Jones (An American Marriage), Nicole Dennis-Benn (Here Comes the Sun), Rebecca Walker (Black, White and Jewish), and more.',
     purchase_url: 'https://www.amazon.com/Well-Read-Black-Girl-Discovering-Ourselves/dp/0525619771/ref=sr_1_1?dchild=1&keywords=well+read+black+girl&qid=1593462194&sr=8-1',
-    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1580245277-518v2gcTL9L.jpg?crop=0.880xw:1xh;center,top&resize=768:*'
+    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1580245277-518v2gcTL9L.jpg?crop=0.880xw:1xh;center,top&resize=768:*',
+    goodreads_url: 'https://www.goodreads.com/book/show/38255372-well-read-black-girl?ac=1&from_search=true&qid=ZLVY6E75k7&rank=1'
   }
 ]
 
-let new_book_btn = document.getElementById('new-book-btn')
+let new_book_btn = document.getElementById('new-book-btn');
+let goodreadsLink = document.getElementById("goodreads-link");
 let bookPurchaseLink = document.getElementById("purchase-link");
 
 let img_created = false;
@@ -84,7 +91,7 @@ new_book_btn.addEventListener("click", (e) => {
     newImg.setAttribute("src", newBook.image_url);
     newImg.setAttribute("class", "book-img");
     bookPurchaseLink.setAttribute("href", newBook.purchase_url);
-    console.log(newBook.purchase_url);
+    goodreadsLink.setAttribute("href", newBook.goodreads_url);
 });
 
 new_book_btn.click()
